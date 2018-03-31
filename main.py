b89e8f080248b97b2d96d64f1bbd32391c70a0f0
Richard Munie <richard.munie@gmail.com>
2018-03-31 16:45:29 -0500

    The Beginnings

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+
+
+   
+@app.route("/")
+def index():
+    encoded_error = request.args.get("error")
+    return render_template('index.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
+
+app.run()
